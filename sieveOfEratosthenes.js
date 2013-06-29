d1ef4278115205a2adcc401333a8e3939a6d2352
chrisbobek <chrisbobek@gmail.com>
2013-06-29 09:41:57 -0700

    Adds comment describing generation of integers

--- sieveOfEratosthenes.js
+++ sieveOfEratosthenes.js
@@ -7,6 +7,7 @@ var out = ;
 var numbers = [];
 var primes = [];
 
+// Create a list of integers from 2 to 100
 function generateNumbers() {
   for(var i = 0; i < 99; i++) {
     numbers[i] = i + 2;
@@ -16,10 +17,6 @@ function generateNumbers() {
 }
 
 function sieve(){
-  for (var i = 0; i <= numbers.length; i++) {
-    //
-  }
-  }
   for (var i = numbers.indexOf(2) + 2; i <= numbers.length; i += 2) {
     numbers[i] = null;
   }
