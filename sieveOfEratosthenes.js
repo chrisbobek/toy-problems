40e36b0c087dd2d8142392661009a839f1bc895a
chrisbobek <chrisbobek@gmail.com>
2013-06-29 19:58:20 -0700

    Renamed a variable

--- sieveOfEratosthenes.js
+++ sieveOfEratosthenes.js
@@ -15,24 +15,24 @@ function generateNumbers() {
 
 // Applies algorithm based on the Sieve of Eratosthenes
 //   to mark non-prime integers in the numbers array
-function sieve(intgerArray, n){
-  for (var i = 0; i < intgerArray.length; i++) {
-    var p = intgerArray[i];
+function sieve(integerArray, n){
+  for (var i = 0; i < integerArray.length; i++) {
+    var p = integerArray[i];
     if (p && (p * p < n)) {
-      for (var j = intgerArray.indexOf(p) + p; j <= intgerArray.length; j += p) {
-        intgerArray[j] = null;
+      for (var j = integerArray.indexOf(p) + p; j <= integerArray.length; j += p) {
+        integerArray[j] = null;
       }
     }
   }
-  numbers = intgerArray;
+  numbers = integerArray;
   return numbers;
 }
 
 // Inserts remaining integers from numbers array into primes array
-function primeNumbers(intgerArray) {
-  for(var i = 0; i <= intgerArray.length; i++){
-    if(intgerArray[i]){
-      primes.push(intgerArray[i]);
+function primeNumbers(integerArray) {
+  for(var i = 0; i <= integerArray.length; i++){
+    if(integerArray[i]){
+      primes.push(integerArray[i]);
     }
   }
   return primes;
